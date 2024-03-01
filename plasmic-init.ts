675c0a9fbaf81826df7a010cbf5f82b31f16ce03
rebrand <annik.2009@gmail.com>
2024-03-01 00:01:50 +0000

    asd

--- plasmic-init.ts
+++ plasmic-init.ts
@@ -3,6 +3,7 @@ import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
 import Stars from "./components/Stars";
 import Embed from "./components/Embed";
 import Slider from "./components/Slider";
+import { HelloWorld } from "./components/HelloWorld";
 
 export const PLASMIC = initPlasmicLoader({
   projects: [
@@ -17,7 +18,7 @@ export const PLASMIC = initPlasmicLoader({
   // For development, you can set preview to true, which will use the unpublished
   // project, allowing you to see your designs without publishing.  Please
   // only use this for development, as this is significantly slower.
-  preview: false,
+  preview: true,
 });
 
 // You can register any code components that you want to use here; see
@@ -39,4 +40,14 @@ PLASMIC.registerComponent(Embed, {
 PLASMIC.registerComponent(Slider, {
   name: "Embed",
   props: {},
+});
+
+// ...
+
+PLASMIC.registerComponent(HelloWorld, {
+  name: "HelloWorld",
+  props: {
+    verbose: "boolean",
+    children: "slot",
+  },
 });
