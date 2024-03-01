ad0ea453d62198610f8913e481ebe7ec8ff5dbfc
rebrand <annik.2009@gmail.com>
2024-03-01 07:29:06 +0000

    test time

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const Stars: React.FC = () => {
-  return <div>Hello, World!</div>;
+  const [currentTime, setCurrentTime] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const newYorkTime = currentTime.toLocaleString('en-US', {
+    timeZone: 'America/New_York',
+  });
+
+  return <div>New York Time: {newYorkTime}</div>;
 };
 
 export default Stars;
